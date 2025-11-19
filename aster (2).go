13a5b40838ecb3ffeb81b3ab0565636b512eb6bf
Hoi1357 <kirillaseev011@gmail.com>
2025-11-19 16:01:44 +0300

    Add files via upload

---
+++ aster (2).go
@@ -0,0 +1,10 @@
+components {
+  id: "script"
+  component: "/stars/aster12.script"
+}
+embedded_components {
+  id: "aster12"
+  type: "factory"
+  data: "prototype: \"/stars/aster.go\"\n"
+  ""
+}
---
+++ factory.go
@@ -0,0 +1,34 @@
+components {
+  id: "script"
+  component: "/stars/factory.script"
+}
+embedded_components {
+  id: "star_factory"
+  type: "factory"
+  data: "prototype: \"/stars/star.go\"\n"
+  ""
+}
+embedded_components {
+  id: "bonus_factory"
+  type: "factory"
+  data: "prototype: \"/stars/bonus_star.go\"\n"
+  ""
+}
+embedded_components {
+  id: "aster12"
+  type: "factory"
+  data: "prototype: \"/stars/aster12.go\"\n"
+  ""
+}
+embedded_components {
+  id: "aster13"
+  type: "factory"
+  data: "prototype: \"/stars/aster13.go\"\n"
+  ""
+}
+embedded_components {
+  id: "super_factory"
+  type: "factory"
+  data: "prototype: \"/stars/super_star.go\"\n"
+  ""
+}
